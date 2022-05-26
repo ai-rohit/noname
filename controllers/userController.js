5ea73eb6e94a103dc16cbe3b9e76e37de6dbd33b
Rohit Shrestha <shrestharohit553@gmail.com>
2022-05-26 12:39:39 +0545

    image upload for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const { User } = require("../models")
+const { CustomError } = require("../helpers");
 
 module.exports = {
     getProfile : (req,res,next)=>{
@@ -13,5 +14,14 @@ module.exports = {
     getUsers: async(req, res, next)=>{
         const users = await User.find();
         return res.status(200).json(users);
+    },
+
+    updateImage: async(req, res, next)=>{
+        if(!req.file){
+            return next(new CustomError("Image is required", 400))
+        }
+        req.user.image = req.file.path.replace("\\", "/");
+        req.user = await req.user.save();
+        return res.status(200).json(req.user)
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,10 @@ const loggedInUser = require("../middlewares/loggedInUser");
 const router = express.Router();
 const wrapAsync = require("../helpers/wrapAsync");
 
+const upload = require("../middlewares/multer")
+
 router.get("/", wrapAsync(userController.getUsers))
 router.get("/me", loggedInUser, userController.getProfile);
+router.put("/me/image", upload.single("image"), loggedInUser, wrapAsync(userController.updateImage))
 
 module.exports = router;
